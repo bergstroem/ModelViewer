0ce5b0f26a2c728c0eeb0281d81cefb4308e31b8
Mattias Bergström <bergstroem1@gmail.com>
2014-01-31 01:58:54 +0100

    Initial Commit

---
+++ GLModelViewer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLModelViewer
+//
+//  Created by Mattias Bergström on 2014-01-31.
+//  Copyright (c) 2014 Mattias Bergström. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
